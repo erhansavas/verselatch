03ad5e06cc917d02be2bdbfc713c384d411ba170
erhansavas <312262902+erhansavas@users.noreply.github.com>
2026-08-13 15:40:56 +0300

    Export analysis backend contract

--- src/verselatch_app/__init__.py
+++ src/verselatch_app/__init__.py
@@ -1,6 +1,14 @@
 # SPDX-FileCopyrightText: 2026 erhansavas
 # SPDX-License-Identifier: GPL-3.0-only
 
+from .backend import AnalysisBackend, AnalysisJob, AnalysisRequest
 from .session import AnalysisResult, SourceIdentity, WorkflowState
 
-__all__ = ["AnalysisResult", "SourceIdentity", "WorkflowState"]
+__all__ = [
+    "AnalysisBackend",
+    "AnalysisJob",
+    "AnalysisRequest",
+    "AnalysisResult",
+    "SourceIdentity",
+    "WorkflowState",
+]
