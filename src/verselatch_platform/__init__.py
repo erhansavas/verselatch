e3fe3173a556a00b7cf3247944d7cf0e7ca67e59
erhansavas <312262902+erhansavas@users.noreply.github.com>
2026-08-13 16:38:40 +0300

    Remove incomplete platform package placeholder

--- src/verselatch_platform/__init__.py
+++
@@ -1,6 +0,0 @@
-# SPDX-FileCopyrightText: 2026 erhansavas
-# SPDX-License-Identifier: GPL-3.0-only
-
-from .worker_process import WorkerBackendError, WorkerCancelled, WorkerProcessBackend
-
-__all__ = ["WorkerBackendError", "WorkerCancelled", "WorkerProcessBackend"]
