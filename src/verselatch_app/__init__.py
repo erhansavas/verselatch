03690b28a613ef17e16119d9377abee0a6f5d7ee
erhansavas <312262902+erhansavas@users.noreply.github.com>
2026-08-13 15:10:53 +0300

    Create toolkit-neutral application package

---
+++ src/verselatch_app/__init__.py
@@ -0,0 +1,6 @@
+# SPDX-FileCopyrightText: 2026 erhansavas
+# SPDX-License-Identifier: GPL-3.0-only
+
+from .session import AnalysisResult, SourceIdentity, WorkflowState
+
+__all__ = ["AnalysisResult", "SourceIdentity", "WorkflowState"]
